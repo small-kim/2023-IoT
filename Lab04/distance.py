2945de14d5171c40e8d93881e145d7ee2a7cf580
SmallKim <iwawy1537@gmil.com>
2023-11-23 19:50:27 +0900

    20231123update3

--- Lab04/distance.py
+++ Lab04/distance.py
@@ -1,14 +1,16 @@
+# Import the RPi.GPIO to use the ultrasonic sensor and PIR sensor
 import RPi.GPIO as gpio
 import paho.mqtt.client as mqtt
 import time
 
+# Define the GPIO pins
 motion = 12
 trig =  20
 echo = 21
 
+# GPIO Settings
 gpio.setwarnings(False)
 gpio.setmode(gpio.BCM)
-
 gpio.setup(motion, gpio.IN)
 gpio.setup(trig, gpio.OUT)
 gpio.setup(echo, gpio.IN)
@@ -20,7 +22,9 @@ mqttc.loop_start()
 
 try:
 	while True:
+		# Check if motion is detected by PIR sensor
 		if gpio.input(motion):
+			# If motion is detected, then measure the distance
 			gpio.output(trig, False)
 			time.sleep(1)
 
@@ -37,8 +41,10 @@ try:
 			distance = pulse_duration * 34000 / 2
 			distance = round(distance, 2)
 		else:
+			# If motion is not deteced, set distance "10000"
 			distance = 10000
 
+		#Print and publish the distance
 		print(distance)
 		infot = mqttc.publish("sensor/distance", distance)
 		infot.wait_for_publish()
--- Lab04/led.py
+++ Lab04/led.py
@@ -1,21 +1,25 @@
+# Import RPi.GPIO to use the led
 import RPi.GPIO as gpio
 import paho.mqtt.client as mqtt
 
+# GPIO Settings
+gpio.setwarnings(False)
 gpio.setmode(gpio.BCM)
 led = [23, 24, 25]
 for i in led:
 	gpio.setup(i, gpio.OUT)
-
-led_color = "0"
 
 def on_connect(client, userdata, flags, rc):
         print("Connected with result code " + str(rc))
         client.subscribe("control/led")
 
 def on_message(client, userdata, msg):
-	global led_color
 	print("Topic: " + msg.topic + ", Led Color: " + msg.payload.decode("utf-8"))
+
+	# Set led_color based on the received MQTT message
 	led_color = msg.payload.decode("utf-8")
+
+	# Leds turn on and off by a message from "control/led"
 	if led_color == "Off":
 		gpio.output(led, False)
 	elif led_color == "green":
